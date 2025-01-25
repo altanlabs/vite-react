49dd8fff980c27ea236e84ce6e820f6458c031b9
Albert Salgueda <albert@altan.ai>
2025-01-26 00:56:01 +0100

    added error boundry

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { Theme } from "@radix-ui/themes";
 import "@radix-ui/themes/styles.css";
 import "./index.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { ThemeProvider } from "@/theme/theme-provider";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.ts";
+import RootBoundary from "./components/errors/RootBoundary";
 
 import { Layout } from "./layout";
 import Index from "./pages/index";
@@ -18,23 +19,31 @@ import { useTheme } from "./theme/use-theme";
 const App = () => {
   const { theme } = useTheme();
 
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout showSidebar={false} showHeader={false} showFooter={false} />,
+      errorElement: <RootBoundary />,
+      children: [
+        {
+          index: true,
+          element: <Index />,
+        },
+        {
+          path: "*",
+          element: <NotFound />,
+        },
+      ],
+    },
+  ]);
+
   return (
     <Provider store={store}>
       <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
         <Theme appearance={theme === "system" ? "light" : theme}>
           <div className={theme}>
             <SidebarProvider>
-              <BrowserRouter>
-                <Routes>
-                  <Route
-                    path="/"
-                    element={<Layout showSidebar={false} showHeader={false} showFooter={false} />}
-                  >
-                    <Route index element={<Index />} />
-                    <Route path="*" element={<NotFound />} />
-                  </Route>
-                </Routes>
-              </BrowserRouter>
+              <RouterProvider router={router} />
             </SidebarProvider>
           </div>
         </Theme>
