12e1a22da8d777b546a00583cd502e98e6d45d23
Albert Salgueda <albert@altan.ai>
2025-02-12 22:09:18 +0100

    non-strict build

--- vite.config.js
+++ vite.config.js
@@ -6,19 +6,21 @@ import react from '@vitejs/plugin-react-swc';
 import * as path from 'path';
 
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src')
-        }
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-    build: {
-        rollupOptions: {
-            onwarn: function (warning, warn) {
-                if (warning.code === 'ESLINT_ERROR')
-                    return;
-                warn(warning);
-            }
-        }
-    }
+  },
+  build: {
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+    rollupOptions: {
+      onwarn: function (warning, warn) {
+        if (warning.code === "ESLINT_ERROR") return;
+        warn(warning);
+      },
+    },
+  },
 });
