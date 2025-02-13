d402cd58a40bb224b6d9077f0b7d53eba7b4e4d0
Altan <albert@altan.ai>
2025-02-13 18:11:53 +0100

    Update routes.tsx

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import Index from "./pages/index";
 import NotFound from "./pages/NotFound";
-import RootBoundary from "./components/errors/RootBoundary";
 
 const routes = [
   {
     path: "/",
-    errorElement: <RootBoundary />,
     children: [
       {
         index: true,
