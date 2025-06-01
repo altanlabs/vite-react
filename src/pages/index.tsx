787c3daf268e42c884cf5c6008a5b594e527a4ff
Albert Salgueda <albert@altan.ai>
2025-06-01 19:18:57 +0200

    upgraded

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
-{/* THIS IS A DEMO SAMPLE WHILE THE APP IS SPINNING, PLEASE REPLACE IT COMPLETELY */}
+{
+  /* THIS IS A DEMO SAMPLE WHILE THE APP IS SPINNING, PLEASE REPLACE IT COMPLETELY */
+}
 
-import { TextShimmer } from "@/components/ui/TextShimmer";
-import { DidYouKnow } from "@/components/ui/DidYouKnow";
-import { SparklesCore } from "@/components/ui/Sparkles";
+import { TextShimmer } from "@/components/example/TextShimmer";
+import { DidYouKnow } from "@/components/example/DidYouKnow";
 
 function App() {
   return (
@@ -17,21 +18,6 @@ function App() {
               Generating interface...
             </TextShimmer>
           </div>
-
-          {/* Gradients */}
-          <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-primary to-transparent h-[2px] w-3/4 blur-sm" />
-          <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-primary to-transparent h-px w-3/4" />
-          <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-primary/80 to-transparent h-[5px] w-1/4 blur-sm" />
-          <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-primary/80 to-transparent h-px w-1/4" />
-
-          <SparklesCore
-            background="transparent"
-            minSize={0.4}
-            maxSize={1}
-            particleDensity={1200}
-            className="w-full h-full"
-            particleColor="currentColor"
-          />
 
           {/* Radial Gradient */}
           <div className="absolute inset-0 w-full h-full bg-background [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,var(--tw-gradient-to))]" />
